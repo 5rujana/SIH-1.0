c31f1e3ab5d9ed0b04b29525d33b8cfbbc9dcdbb
Amaymani <amaytripathi19@gmail.com>
2023-09-20 02:29:02 +0530

    npm modules and routing added

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const findOrCreate = require("mongoose-findorcreate");
+const app = express();
+
+mongoose.connect("mongodb://127.0.0.1:27017/usersihDB", {
+  useNewUrlParser: true,
+});
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: "Our little secret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(function (user, cb) {
+  process.nextTick(function () {
+    cb(null, { id: user.id, username: user.username, name: user.name });
+  });
+});
+
+passport.deserializeUser(function (user, cb) {
+  process.nextTick(function () {
+    return cb(null, user);
+  });
+});
+
+app.get("/", (req, res) => {
+  res.render("login");
+});
+
+app.listen(3000, function (req, res) {
+  console.log("Server is connected at port 3000");
+});
