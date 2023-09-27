3f99c88427bc4a00a815609ba30068c91e01b92b
Chaganti Srujana Gayatri <gayatri.22bcb7012@vitapstudent.ac.in>
2023-09-27 13:17:28 +0530

    commit

--- app.js
+++ app.js
@@ -1,53 +1,53 @@
 const express = require("express");
 const ejs = require("ejs");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const session = require("express-session");
-const passport = require("passport");
-const passportLocalMongoose = require("passport-local-mongoose");
-const findOrCreate = require("mongoose-findorcreate");
+// const session = require("express-session");
+// const passport = require("passport");
+// const passportLocalMongoose = require("passport-local-mongoose");
+// const findOrCreate = require("mongoose-findorcreate");
 const app = express();
 
-mongoose.connect("mongodb://127.0.0.1:27017/userDB", {
-  useNewUrlParser: true,
-});
+// mongoose.connect("mongodb://127.0.0.1:27017/userDB", {
+//   useNewUrlParser: true,
+// });
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(
-  session({
-    secret: "Our little secret",
-    resave: false,
-    saveUninitialized: false,
-  })
-);
+// app.use(
+//   session({
+//     secret: "Our little secret",
+//     resave: false,
+//     saveUninitialized: false,
+//   })
+// );
 
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
-const userSchema = new mongoose.Schema({
-  email: String,
-  password: String,
-});
+// const userSchema = new mongoose.Schema({
+//   email: String,
+//   password: String,
+// });
 
-userSchema.plugin(passportLocalMongoose);
-userSchema.plugin(findOrCreate);
-const User = new mongoose.model("User", userSchema);
+// userSchema.plugin(passportLocalMongoose);
+// userSchema.plugin(findOrCreate);
+// const User = new mongoose.model("User", userSchema);
 
-passport.use(User.createStrategy());
+// passport.use(User.createStrategy());
 
-passport.serializeUser(function (user, cb) {
-  process.nextTick(function () {
-    cb(null, { id: user.id, username: user.username, name: user.name });
-  });
-});
+// passport.serializeUser(function (user, cb) {
+//   process.nextTick(function () {
+//     cb(null, { id: user.id, username: user.username, name: user.name });
+//   });
+// });
 
-passport.deserializeUser(function (user, cb) {
-  process.nextTick(function () {
-    return cb(null, user);
-  });
-});
+// passport.deserializeUser(function (user, cb) {
+//   process.nextTick(function () {
+//     return cb(null, user);
+//   });
+// });
 
 app.get("/", (req, res) => {
   res.render("home");
